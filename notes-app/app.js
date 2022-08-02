bd20c2ea0abbd98c2e3464f4ab5b12d9e576954f
Serhii Holub <serhii.holub@valtech.com>
2022-08-02 16:21:10 +0300

    Importing npm modules

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,12 @@
+const validator = require('validator')
 const getNotes = require('./notes.js')
 
 const msg = getNotes()
 
 console.log(msg)
+
+console.log(validator.isEmail('serhii@test.com'))
+console.log(validator.isURL('test.com'))
 
 // const add = require('./utils.js')
 // const sum = add(4, -2)
