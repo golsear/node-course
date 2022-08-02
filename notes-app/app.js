0d40b545d9c47f21171dba944ada1d680d20b972
Serhii Holub <serhii.holub@valtech.com>
2022-08-02 16:57:56 +0300

    Global npm modules and nodemon

--- notes-app/app.js
+++ notes-app/app.js
@@ -9,6 +9,7 @@ console.log(msg)
 console.log(validator.isEmail('serhii@test.com'))
 console.log(validator.isURL('test.com'))
 console.log(chalk.green.bold.inverse('Success!'))
+console.log(chalk.red.bold.inverse('Error!'))
 
 // const add = require('./utils.js')
 // const sum = add(4, -2)
