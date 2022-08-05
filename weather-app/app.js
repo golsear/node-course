e4aa7c099caed04953aaad6b1399c5914019d8b9
Serhii Holub <serhii.holub@valtech.com>
2022-08-05 11:36:13 +0300

    Making HTTP Requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,15 @@
-console.log('Starting')
+const axios = require('axios')
 
-setTimeout(() => {
-    console.log('2 Second Timer')
-}, 2000)
+const url = 'http://api.weatherstack.com/current'
 
-setTimeout(() => {
-    console.log('0 Second Timer')
-}, 0)
-
-console.log('Stopping')
+const params = {
+    access_key: 'b2ecd0581854b1a6afd6f2a2f98f0ea3',
+    query: '49.750,27.217'
+  }
+  
+axios.get(url, { params })
+.then(response => {
+    console.log(response.data.current)
+}).catch(error => {
+    console.log(error)
+})
