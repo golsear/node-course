da7c05cb36c051f8438fdb74a71709c77539f36b
Serhii Holub <serhii.holub@valtech.com>
2022-08-05 17:21:27 +0300

    Customizing HTTP Requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,12 +4,15 @@ const url = 'http://api.weatherstack.com/current'
 
 const params = {
     access_key: 'b2ecd0581854b1a6afd6f2a2f98f0ea3',
-    query: '49.750,27.217'
+    query: '49.750,27.217',
+    units: 'm'
   }
   
 axios.get(url, { params })
 .then(response => {
+    const current = response.data.current 
     console.log(response.data.current)
+    console.log(`${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees out. It feels like ${current.feelslike} degrees out.`)
 }).catch(error => {
     console.log(error)
 })
