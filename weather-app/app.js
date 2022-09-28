9d7fcd05ff573ac9c411c4c4ef5f211157e28394
Serhii Holub <serhii.holub@valtech.com>
2022-09-28 16:32:48 +0300

    Error handling

--- weather-app/app.js
+++ weather-app/app.js
@@ -6,13 +6,16 @@ const params = {
     access_key: 'b2ecd0581854b1a6afd6f2a2f98f0ea3',
     query: '49.750,27.217',
     units: 'm'
-  }
+}
   
 axios.get(url, { params })
 .then(response => {
-    const current = response.data.current 
-    console.log(response.data.current)
-    console.log(`${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees out. It feels like ${current.feelslike} degrees out.`)
+    if (response.data.error) {
+        console.log(response.data.error.info)
+    } else {
+        const current = response.data.current 
+        console.log(`${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees out. It feels like ${current.feelslike} degrees out.`)
+    }
 }).catch(error => {
-    console.log(error)
+    console.log('Unable to connect to location services!')
 })
